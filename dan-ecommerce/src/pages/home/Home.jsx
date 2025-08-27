28e1e734fa604b62b0cc885ac94e7d4313a3e811
Yahya <yahyatp.cybexel@gmail.com>
2025-08-27 10:15:07 +0530

    first commit

---
+++ dan-ecommerce/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './homeitems/Hero'
+
+function Home() {
+  return (
+    <div className='px-[3.8%] pt-[1.5%] pb-[4%] bg-white w-[100%] h-[100]'>
+        <div className='bg-white'>
+    
+    <Hero />
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
