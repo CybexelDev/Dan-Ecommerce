aa00cf097db614661078590048f1d5abcc55a6c8
N-meapp <developersnme@gmail.com>
2025-10-14 15:49:00 +0530

    ashif commit

--- dan-ecommerce/src/pages/home/homeitems/TestimonalClients.jsx
+++ dan-ecommerce/src/pages/home/homeitems/TestimonalClients.jsx
@@ -1,3 +1,42 @@
+// import React from "react";
+// import clientlogo from "../../../assets/images/testimonals/clientlogo.png"; // fallback
+
+// function TestimonalClients({ clients }) {
+//   // Duplicate the clients array to create seamless loop
+//   const duplicatedClients = [...clients, ...clients];
+
+//   return (
+//     <div className="w-full h-[18%] flex flex-col justify-between">
+//       <div className="flex justify-center w-[42%] text-[1vw] mx-auto">
+//         <p>Feature client logos to build trust and credibility for your brand:</p>
+//       </div>
+      
+//       {/* Carousel Container */}
+//       <div className="w-full overflow-hidden">
+//         <div className="w-[42%] mx-auto relative">
+//           {/* Carousel Track */}
+//           <div className="flex gap-[1.8vw] py-[.6vh] animate-scroll">
+//             {duplicatedClients.map((client, index) => (
+//               <img
+//                 key={index}
+//                 className="w-[15%] h-full object-contain flex-shrink-0"
+//                 src={client?.image[0]}
+//                 alt={client?.tittle}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default TestimonalClients;
+
+
+
+
+
 import React from "react";
 import clientlogo from "../../../assets/images/testimonals/clientlogo.png"; // fallback
 
